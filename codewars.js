53812e05ffb76b43e7ed8928f52e19ee59b1fd09
Fabian <125907927+mskiFabian@users.noreply.github.com>
2023-05-02 23:31:07 +0200

    Add files via upload

---
+++ codewars.js
@@ -0,0 +1,20 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+
+function solution(string) {
+    let newString = ''
+    for(letter of string) {
+        if(letter == letter.toUperCase()) {
+            newString += ' '
+            newString += letter
+        } else {
+            newString += letter
+        }
+    } return newString
+
+}
